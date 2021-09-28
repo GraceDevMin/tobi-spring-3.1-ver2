d31d5ee104a721734db3c2fea3d068cfaa554ed4
Eunhye <olivgrece@gmail.com>
2021-09-28 20:55:30 +0900

    210928 Tue

--- src/main/java/com/tobi/dao/UseDaoTest.java
+++ src/main/java/com/tobi/dao/UseDaoTest.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.dao.EmptyResultDataAccessException;
+
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 
@@ -20,21 +22,34 @@ public class UseDaoTest {
 
         UserDao dao = context.getBean("userDao", UserDao.class);
 
+        User user1 = new User("gyumee", "박성철", "Springno1");
+        User user2 = new User("leegw700", "이길원", "Springno2");
+
         dao.deleteAll();
         assertThat(dao.getCount(), is(0));
 
-        User user = new User();
-        user.setId("gyumee");
-        user.setName("박성철");
-        user.setPassword("springno1");
+        dao.add(user1);
+        dao.add(user2);
+        assertThat(dao.getCount(), is(2));
 
-        dao.add(user);
-        assertThat(dao.getCount(), is(1));
+        User userGet1 = dao.get(user1.getId());
+        assertThat(userGet1.getName(), is(user1.getName()));
+        assertThat(userGet1.getPassword(), is(user1.getPassword()));
 
-        User user2 = dao.get(user.getId());
+        User userGet2 = dao.get(user2.getId());
+        assertThat(userGet2.getName(), is(user2.getName()));
+        assertThat(userGet2.getPassword(), is(user2.getPassword()));
+    }
+    
+    @Test(expected = EmptyResultDataAccessException.class)  //테스트중 발생할 것으로 기대되는 예외 클래스 지정
+    public void getUserFailure() throws SQLException, ClassNotFoundException {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(DaoFactory.class);
+        
+        UserDao dao = context.getBean("userDao", UserDao.class);
+        dao.deleteAll();
+        assertThat(dao.getCount(), is(0));
 
-        assertThat(user2.getName(), is(user.getName()));
-        assertThat(user2.getPassword(), is(user.getPassword()));
-
+        dao.get("unknown_id");  //이 메소드 실행중 예외 발생. 예외발생X면 테스트실패
     }
 }
